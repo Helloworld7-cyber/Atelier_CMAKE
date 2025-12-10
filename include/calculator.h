3b40465127b3e3655811d39322a4d331f0a5162d
Helloworld7-cyber <Matteobray77@gmail.com>
2025-12-10 10:04:56 +0100

    Add car function declaration to calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -2,3 +2,13 @@ double _add(double a, double b);
 double _sub(double a, double b);
 double _mul(double a, double b);
 double _div(double a, double b);
+// include/calculator.h
+
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+// ... (fonctions existantes)
+
+int car(int a); // <-- AJOUT
+
+#endif // CALCULATOR_H
