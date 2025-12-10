1de890f07d16f091f41282ad6a102efd0fda3e11
Helloworld7-cyber <Matteobray77@gmail.com>
2025-12-10 10:05:42 +0100

    Implement car function to calculate square of a number
    Added a new function 'car' that returns the square of an integer.

--- src/lib/calculator.c
+++ src/lib/calculator.c
@@ -15,4 +15,13 @@ double _mul(double a, double b) {
 double _div(double a, double b) {
 	return a/b;
 }
-	
+// src/lib/calculator.c
+
+#include "calculator.h" // ⬅️ ASSUREZ-VOUS D'AVOIR LE .h ET NON LE .c !
+
+// ... (implémentations existantes)
+
+// Nouvelle fonction 'car'
+int car(int a) {
+    return a * a;
+}	
